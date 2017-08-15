961e32b0e20513162ecf55b1f3a22760085c514e
bruce.m.li@microcorecn.com <bruce.m.li@microcorecn.com>
2017-08-15 18:05:43 +0800

    头部页面（完成），底部（未完成）

---
+++ js/header.js
@@ -0,0 +1,26 @@
+
+function select_header(id){
+	if(id){
+		$('#'+id).addClass('bg_color').css("color","#fff");
+	}else{
+		$('#home').addClass('bg_color').css("color","#fff");
+	}
+	
+}
+$(function(){
+	var id = $.cookie("id");
+	select_header(id);
+	$('#header li a').click(function(){
+		var id=$(this).attr("id");
+		if(!id){
+			id=$(this).data("id");
+		}
+		$.cookie("id",id);
+	});
+	
+	$('#header li').hover(function(){
+		$(this).find("div").show();
+	},function(){
+		$(this).find("div").hide()
+	});
+});
